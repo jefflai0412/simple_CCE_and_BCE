d20d7daded5a21d58fcd93677331481514a938bb
unknown <jefflai0412@gmail.com>
2024-12-22 14:02:07 +0800

    categorical cross entropy finished

--- NN_mnist.py
+++ NN_mnist.py
@@ -9,6 +9,20 @@ from tensorflow.keras.utils import to_categorical
 def MY_L1(yHat, y):
     return 0.5 * K.mean(K.square(y - yHat))
 
+def cross_entropy(yHat, y):
+    # Add a small epsilon to yHat to prevent log(0)
+    epsilon = K.epsilon()  # Small value for numerical stability
+    yHat = K.clip(yHat, epsilon, 1.0 - epsilon)
+    print("yhat shape:", yHat.shape)
+    
+    # Compute categorical cross-entropy
+    loss = -K.mean(K.sum(y * K.log(yHat), axis=-1))
+    return loss
+
+
+def binary_cross_entropy(yHat, y):
+    return K.binary_crossentropy(y, yHat)
+
 
 # 載入MNIST資料集
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data( )
@@ -17,7 +31,7 @@ def MY_L1(yHat, y):
 network = Sequential( )
 network.add( Dense( 512, activation ='relu', input_shape = ( 784, ) ) )
 network.add( Dense( 10, activation = 'softmax' ) )
-network.compile( optimizer = 'rmsprop', loss = MY_L1, metrics = ['accuracy'] )
+network.compile( optimizer = 'rmsprop', loss = cross_entropy, metrics = ['accuracy'] )
 print( network.summary() )
 # 資料前處理
 train_images = train_images.reshape( ( 60000, 28 * 28 ) )
@@ -28,7 +42,7 @@ train_labels = to_categorical( train_labels )
 test_labels = to_categorical( test_labels )
 
 # 訓練階段
-network.fit( train_images, train_labels, epochs = 5, batch_size = 200 )
+network.fit( train_images, train_labels, epochs = 5, batch_size = 400 )
 # 測試階段
 test_loss, test_acc = network.evaluate( test_images, test_labels )
 print( "Test Accuracy:", test_acc )
